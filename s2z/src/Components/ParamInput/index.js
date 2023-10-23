af9495ac03c579b12754b49c89335fc8fddc0dd7
trisrosco <trisrosco@gmail.com>
2023-10-23 15:31:58 +0100

    MM/YYYY picker installed and added

--- s2z/src/Components/ParamInput/index.js
+++ s2z/src/Components/ParamInput/index.js
@@ -13,49 +13,74 @@ import {
   TextField,
   IconButton,
   Paper,
+  Stack,
 } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import AddIcon from "@mui/icons-material/Add";
+import AutoGraphIcon from "@mui/icons-material/AutoGraph";
+import { DateField } from "@mui/x-date-pickers/DateField";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 function ParamInput() {
   const [lineCount, setLineCount] = useState(1);
+  const [lines, setLines] = useState([{ id: 1, monthYear: "", numTrees: "" }]);
 
   const handleAddLine = () => {
-    setLineCount((prevCount) => prevCount + 1);
+    const newLineCount = lineCount + 1;
+    setLineCount(newLineCount);
+    setLines([...lines, { id: newLineCount, monthYear: "", numTrees: "" }]);
+  };
+
+  const handleDeleteLine = (id) => {
+    const updatedLines = lines.filter((line) => line.id !== id);
+    setLines(updatedLines);
+  };
+
+  const handleLineChange = (id, key, value) => {
+    const updatedLines = lines.map((line) =>
+      line.id === id ? { ...line, [key]: value } : line
+    );
+    setLines(updatedLines);
   };
 
   const renderLines = () => {
-    const lines = [];
-    for (let i = 1; i <= lineCount; i++) {
-      lines.push(
-        <div key={i}>
-          <ListItem>
-            <ListItemText primary={i} />
-            <TextField
-              id={`month-year-${i}`}
+    return lines.map((line) => (
+      <div key={line.id}>
+        <ListItem>
+          <ListItemText primary={line.id} />
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DateField
               label="Month & Year"
+              format="MM-YYYY"
               variant="standard"
             />
-            <TextField
-              id={`num-trees-${i}`}
-              label="Number of Trees"
-              variant="standard"
-            />
-            <IconButton aria-label="delete">
-              <DeleteIcon />
-            </IconButton>
-          </ListItem>
-          {i !== lineCount && <Divider />}
-        </div>
-      );
-    }
-    return lines;
+          </LocalizationProvider>
+          <TextField
+            id={`num-trees-${line.id}`}
+            label="Number of Trees"
+            variant="standard"
+            value={line.numTrees}
+            onChange={(e) =>
+              handleLineChange(line.id, "numTrees", e.target.value)
+            }
+          />
+          <IconButton
+            aria-label="delete"
+            onClick={() => handleDeleteLine(line.id)}
+          >
+            <DeleteIcon />
+          </IconButton>
+        </ListItem>
+        {line.id !== lineCount && <Divider />}
+      </div>
+    ));
   };
 
   return (
-    <Paper elevation={5} className="param-input">
+    <Paper elevation={10} className="param-input">
       <Paper elevation={3} className="param-input-header">
-        <FormControl>
+        <FormControl variant="filled">
           <InputLabel>Country</InputLabel>
           <Select className="selector" label="Country">
             <MenuItem value={15.52}>United States</MenuItem>
@@ -63,7 +88,7 @@ function ParamInput() {
             <MenuItem value={9.44}>Germany</MenuItem>
           </Select>
         </FormControl>
-        <FormControl>
+        <FormControl variant="filled">
           <InputLabel>Frequency</InputLabel>
           <Select className="selector" label="Frequency">
             <MenuItem value={0}>Weekly</MenuItem>
@@ -72,17 +97,25 @@ function ParamInput() {
           </Select>
         </FormControl>
       </Paper>
-      <div className="param-input-grid">
-        <Table>{renderLines()}</Table>
+      <Table>{renderLines()}</Table>
+      <Stack className="param-input-buttons">
         <Button
-          color="secondary"
+          color="primary"
           startIcon={<AddIcon />}
           variant="contained"
           onClick={handleAddLine}
         >
           <span>Add line</span>
         </Button>
-      </div>
+        <Button
+          color="secondary"
+          startIcon={<AutoGraphIcon />}
+          variant="contained"
+          onClick={console.log("Generate Graph")}
+        >
+          <span>Generate Graph</span>
+        </Button>
+      </Stack>
     </Paper>
   );
 }
