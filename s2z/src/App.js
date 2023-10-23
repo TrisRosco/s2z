4f59df8fa68aa8c9a70e446a1bdf718466e5261f
trisrosco <trisrosco@gmail.com>
2023-10-23 11:21:38 +0100

    installed MUI

--- s2z/src/App.js
+++ s2z/src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Button from '@mui/material/Button';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h2>Carbon Offset Simulator</h2>
       </header>
     </div>
   );
