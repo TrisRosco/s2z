0cd2722404cc9c3dbd6832bf4f3cd1a6dd4a2582
trisrosco <trisrosco@gmail.com>
2023-10-23 12:05:13 +0100

    Added more client functions for sign-in

--- s2z/src/Models/client.js
+++ s2z/src/Models/client.js
@@ -5,25 +5,60 @@ const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
+// sign in user
 export async function SignInUser(email, password) {
-    try {
-        const { error } = await supabase.auth.signIn({
-            email: email,
-            password: password,
-        });
+  try {
+    const { error } = await supabase.auth.signIn({
+      email: email,
+      password: password,
+    });
 
-        if (error) {
-            console.log("Sign in error: ", error);  
-            return false;
-        } else {
-            return true;
-        }
+    if (error) {
+      console.log("Sign in error: ", error);
+      return false;
+    } else {
+      return true;
     }
-    catch (error) {
-        console.log("Sign in error: ", error);
-        return false;
-    }
+  } catch (error) {
+    console.log("Sign in error: ", error);
+    return false;
+  }
 }
 
+// Check if a user is signed in
+export async function IsUserSignedIn() {
+  try {
+    const { data, error } = await supabase.auth.getSession();
 
+    if (error) {
+      console.log(error);
+      return false;
+    } else {
+      if (data.session) {
+        console.log("A user is signed in.", data);
+        return true;
+      } else {
+        console.log("A user is not signed in.", data);
+        return false;
+      }
+    }
+  } catch (error) {
+    // Handle any other errors that may occur
+    console.error(error);
+    return false;
+  }
+}
 
+// Get current user id
+export async function GetCurrentUserID() {
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  return user.id;
+}
+
+// Sign out
+
+export async function SignOutUser() {
+  await supabase.auth.signOut();
+}
