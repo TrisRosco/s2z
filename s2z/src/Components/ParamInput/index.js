ceed6bca69b34477606b0fbc33aa28c79dc0f04a
trisrosco <trisrosco@gmail.com>
2023-10-23 14:46:48 +0100

    Addline button works

--- s2z/src/Components/ParamInput/index.js
+++ s2z/src/Components/ParamInput/index.js
@@ -1,26 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import Select from "@mui/material/Select";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
-import Divider from "@mui/material/Divider";
-import TextField from "@mui/material/TextField";
-import IconButton from "@mui/material/IconButton";
+import {
+  Button,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+  List,
+  ListItem,
+  ListItemText,
+  Divider,
+  TextField,
+  IconButton,
+  Paper,
+} from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
-import Paper from "@mui/material/Paper";
-
+import AddIcon from "@mui/icons-material/Add";
 
 function ParamInput() {
+  const [lineCount, setLineCount] = useState(1);
+
+  const handleAddLine = () => {
+    setLineCount((prevCount) => prevCount + 1);
+  };
+
+  const renderLines = () => {
+    const lines = [];
+    for (let i = 1; i <= lineCount; i++) {
+      lines.push(
+        <div key={i}>
+          <ListItem>
+            <ListItemText primary={i} />
+            <TextField id={`month-year-${i}`} label="Month & Year" variant="standard" />
+            <TextField id={`num-trees-${i}`} label="Number of Trees" variant="standard" />
+            <IconButton aria-label="delete">
+              <DeleteIcon />
+            </IconButton>
+          </ListItem>
+          {i !== lineCount && <Divider />}
+        </div>
+      );
+    }
+    return lines;
+  };
+
   return (
     <Paper elevation={5} className="param-input">
       <Paper elevation={3} className="param-input-header">
         <FormControl>
           <InputLabel>Country</InputLabel>
-          <Select id="country-select" label="Country">
+          <Select className="selector" label="Country">
             <MenuItem value={15.52}>United States</MenuItem>
             <MenuItem value={5.55}>United Kingdom</MenuItem>
             <MenuItem value={9.44}>Germany</MenuItem>
@@ -28,33 +57,23 @@ function ParamInput() {
         </FormControl>
         <FormControl>
           <InputLabel>Frequency</InputLabel>
-          <Select id="country-select" label="Country">
-            <MenuItem value={0}>Weeky</MenuItem>
+          <Select  label="Frequency">
+            <MenuItem value={0}>Weekly</MenuItem>
             <MenuItem value={0}>Monthly</MenuItem>
-            <MenuItem value={0}>Anually</MenuItem>
+            <MenuItem value={0}>Annually</MenuItem>
           </Select>
         </FormControl>
       </Paper>
       <div className="param-input-grid">
-        <List>
-          <ListItem>
-            <ListItemText primary="1" />
-            <TextField
-              id="standard-basic"
-              label="Month & Year"
-              variant="standard"
-            />
-            <TextField
-              id="standard-basic"
-              label="Number of Trees"
-              variant="standard"
-            />
-            <IconButton aria-label="delete">
-              <DeleteIcon />
-            </IconButton>
-          </ListItem>
-          <Divider />
-        </List>
+        <List>{renderLines()}</List>
+        <Button
+          color="secondary"
+          startIcon={<AddIcon />}
+          variant="contained"
+          onClick={handleAddLine}
+        >
+          <span>Add line</span>
+        </Button>
       </div>
     </Paper>
   );
